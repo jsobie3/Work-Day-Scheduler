c1ed01bbfe5ee73d1e43bc889e22eeafbc481e01
jsobie3 <jakub.sobieraj01@gmail.com>
2023-01-03 22:39:34 -0600

    fixed work day

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,19 @@
-console.log("hello")
-
-
 // -- DayJS Time -- //
-function currentTime(){
-  let currentDay = dayjs().format('MMM DD, YYYY:   h:mm a');
-  $('#currentDay').text(currentDay);
-}
+var currentDate = dayjs().format("dddd, MMM Do YYYY: h:mm a");
+var todayDate = document.getElementById("currentDay");
+var currentHour = dayjs().format("HH")
 
-setInterval(currentTime,1000)
-
-var hour = dayjs().hour()
+var saveButtons = document.querySelectorAll(".saveBtn")
 
 
 
 function colorTime(){
 
 $('.hourblock').each(function ()  {
-  console.log('test')
+  
     var currentHour = dayjs().format('h')
     var hourBlock = parseInt($(this).attr('id').split('-')[1]);
-  console.log(currentHour)
+  
     if (currentHour === hourBlock) {
     $(this).addClass('present')
     }
@@ -36,11 +30,9 @@ $('.hourblock').each(function ()  {
 colorTime();
 
 // -- Save Button -- //
-//
 $(".saveBtn").on("click", function saveInput(event)
 {
-event.preventDefault
-console.log("hello");
+
 event.preventDefault();
 let tasks = $(this).siblings(".description").val();
 let time = $(this).parents().attr("id").split("-")[1];
@@ -48,29 +40,4 @@ localStorage.setItem(time, tasks)
 })
 
 
-
-
-
-
-// -- Color Change Based On Time -- //
-//
-// function timeQuery (){
-//   console.log("test")
-
-//   var currentHour = dayjs().format('h')
-//   var hourBlock = $(this).attr("id").split("-")[1];
-
-  
-// console.log(currentHour)
-
-//   if (currentHour === hourBlock) {
-//   $(this).addClass("present")    
-//   }
-//   else if(currentHour >= hourBlock) {
-//     $(this).addClass ("future")
-//   }
-//   else {
-//     $(this).addClass("past")
-//   }
-// }
-// timeQuery();
+setInterval(timeUpdate, 1000)
